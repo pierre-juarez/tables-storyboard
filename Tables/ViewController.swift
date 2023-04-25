9956d839f214ce69eedafe5c8a547c16aa11c101
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-25 00:22:44 -0500

    [add] - Tableview 🤩

---
+++ Tables/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Tables
+//
+//  Created by Pierre Juarez U. on 24/04/23.
+//
+
+import UIKit
+
+struct listModel {
+    let name: String
+    let email: String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var contactTable: UITableView!
+    
+    var list = [listModel]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        contactTable.delegate = self
+        contactTable.dataSource = self
+        
+        let elem1 = listModel(name: "Pierre Juarez", email: "jp@email.dev.com")
+        let elem2 = listModel(name: "Jose Ramos", email: "jsrpos@email.dev.com")
+        let elem3 = listModel(name: "Benito Juarez", email: "jp@email.dev.com")
+        let elem4 = listModel(name: "Benito Juarez", email: "jp@email.dev.com")
+        
+        list.append(elem1)
+        list.append(elem2)
+        list.append(elem3)
+        list.append(elem4)
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellTableContact", for: indexPath)
+        let item = list[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = item.email
+        cell.imageView?.image = UIImage(systemName: "person.crop.circle.fill")
+        return cell
+    }
+    
+    
+}
